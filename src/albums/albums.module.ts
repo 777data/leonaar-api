941f1e84130cce85f2aa30ab4c1746323bbef844
David <davidtant77@gmail.com>
2025-08-28 18:38:55 +0200

    Bug

--- src/albums/albums.module.ts
+++ src/albums/albums.module.ts
@@ -4,9 +4,13 @@ import { AlbumsService } from './albums.service';
 import { AlbumsController } from './albums.controller';
 import { Album } from './entities/album.entity';
 import { Photo } from './entities/photo.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Album, Photo])],
+  imports: [
+    TypeOrmModule.forFeature([Album, Photo]),
+    UsersModule,
+  ],
   controllers: [AlbumsController],
   providers: [AlbumsService],
 })
