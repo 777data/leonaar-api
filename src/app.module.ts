54ca0b73110dc6d7cfa8c2af962118948ec54f51
David <davidtant77@gmail.com>
2025-08-28 17:59:14 +0200

    Pb middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { databaseConfig } from './config/database.config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { UploadSecurityMiddleware } from './common/middleware/upload-security.middleware';
+import { JwtModule } from '@nestjs/jwt';
+import { envConfig } from './config/env.config';
 
 @Module({
   imports: [
@@ -17,18 +19,24 @@ import { UploadSecurityMiddleware } from './common/middleware/upload-security.mi
       envFilePath: '.env',
     }),
     TypeOrmModule.forRoot(databaseConfig),
+    JwtModule.register({
+      secret: envConfig.jwt.secret,
+      signOptions: { expiresIn: '24h' },
+    }),
     AlbumsModule,
     AuthModule,
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UploadSecurityMiddleware],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(json({ limit: '50mb' }), urlencoded({ limit: '50mb', extended: true }))
-      .forRoutes('*')
+      .forRoutes('*');
+
+    consumer
       .apply(UploadSecurityMiddleware)
       .forRoutes('/uploads/*');
   }
